5e4ed1ac264620e54c70ab7779dd5a57f75a90d2
robin <robin@punnoose>
2026-04-13 16:41:52 -0500

    adjacent tag skip vacant

--- config.h
+++ config.h
@@ -87,6 +87,10 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY|ControlMask,           XK_l,      viewnext,       {0} },
+	{ MODKEY|ControlMask,           XK_h,      viewprev,       {0} },
+	{ MODKEY|ShiftMask,             XK_l,      tagtonext,      {0} },
+	{ MODKEY|ShiftMask,             XK_h,      tagtoprev,      {0} },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
